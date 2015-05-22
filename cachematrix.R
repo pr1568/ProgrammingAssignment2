ea46fb48af8b5870950da2fcf412360991aef984
Ranjan Gupta <pr1568@att.com>
2015-05-22 13:56:16 -0500

    Submitting the Assignment
    Initial code update with solution and step by step comments

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,30 @@
 
 ## Write a short comment describing this function
 
+## Return a list containing functions to set the different aspect of Caching the Matrix, 
+## follwing aspect will be covered in this function
+##    1.set the value of the vector
+##    2.get the value of the vector
+##    3.set the value of the mean
+##    4.get the value of the mean
+## This function will be used with cacheSolve function as an input
+
+
 makeCacheMatrix <- function(x = matrix()) {
+  ## initialize the inv variable
+  inv <- NULL
+  set <- function(y) {
+    # use `<<-` or assignment operator to assign a value to an object 
+    # in global environment
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse 
+  getinv <- function() inv
+  
+  ## return the list which contains all the four vectors
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 
 }
 
